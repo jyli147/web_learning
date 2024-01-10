f88441217da70e0fa015176481bae4bf45922387
Юля <jyli147@mail.ru>
2024-01-10 16:40:27 +0300

    Правки

--- task/index.js
+++ task/index.js
@@ -1,3 +1,5 @@
+// Модальное окно
+
 document.getElementById("open-modal").addEventListener("click", function () {
     document.getElementById("modal").classList.add("open")
 });
@@ -22,5 +24,92 @@ function clearTasks() {
 // Добавляем обработчик события клика по кнопке очистки
 clearButton.addEventListener('click', clearTasks);
 
+
+
 // Добавляем задачи
-const add = document.getElementById(`add`)
+const form = document.querySelector(`#form`)
+const add = document.getElementById('add');
+const input = document.getElementById(`#input`);
+const tasksList = document.getElementById(`#taskList`);
+
+form.addEventListener(`submit`, function (event) {
+    // Отменяем отправку формы
+    event.preventDefault();
+    //    Достаем текст из поля ввода задачи
+    const taskText = input.value;
+    console.log(taskText);
+    // Разметка для задачи
+    const taskHtml = `<div class="task">
+    <label class="form">
+        <input type="checkbox" class="real-checkbox">
+        <span class="custom-checkbox"></span>
+        <p class="subtitle">${taskText}</p>
+    </label>
+    <button type="button"
+        class="button-right-panel surface-button button-urgent">Urgent</button>
+</div>`
+    // Добавить на страницу
+    tasksList.insertAdjacentHTML(`beforeend`, taskHtml);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Task {
+//     constructor(id, description, categories, isCompleted = false) {
+//         this.id = id;
+//         this.description = description;
+//         this.categories = categories;
+//         this.isCompleted = isCompleted;
+//     }
+
+//     toggleCompleted() {
+//         this.isCompleted = !this.isCompleted;
+//     };
+
+//     toString() {
+//         return `id=${this.id}, title=${this.title}, description=${this.description}, isCompleted=${this.isCompleted}`;
+//     }
+// };
+
+
+// class TaskList {
+//     #tasks = [];
+
+//     addTask(description, categories,) {
+//         let task = new Task(this.#tasks.length + 1, description, categories,);
+//         this.#tasks.push(task);
+//         return task;
+//     }
+//     // addTask(taskDescription) {
+//     //     let task = new Task(taskDescription);
+//     //     this.#tasks.push(task);
+//     //     return task;
+//     // }
+// }
+
+
+// let myTasksList = new TaskList();
+
+// add.onclick = () => {
+//     let task = input.value;
+//     if (task) {
+//         console.log(myTasksList.addTask());
+//     }
+// }

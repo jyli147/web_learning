ee9428e8abc435ed286003ed231808ca3012bfda
Юля <jyli147@mail.ru>
2024-06-15 13:20:31 +0300

    правки

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './components/App.css';
 import Header from './components/header';
 import Form from './components/Form';
@@ -7,6 +7,15 @@ import Fotter from './components/Fotter';
 
 
 function App() {
+  const getFilms = async () => {
+    const response = await fetch('https://www.omdbapi.com/?i=tt3896198&apikey=24a33c8f&t=matrix')
+    const data = await response.json();
+    console.log(data)
+
+  }
+  useEffect(() => {
+    getFilms();
+  }, [])
   return (
     <div className="App">
       <Header/>

324bfe1818c76cdc7871e8a2d2860403fa60a2bd
Юля <jyli147@mail.ru>
2024-11-06 23:17:13 +0300

    ЕНастройка gulp

---
+++ web/gulpfile.js
@@ -0,0 +1,43 @@
+const { src, dest, watch, parallel } = require("gulp");
+
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const browserSync = require("browser-sync").create();
+
+function styles() {
+  return src("app/scss/style.scss")
+    .pipe(concat("style.min.css"))
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
+}
+
+function script() {
+  return src("app/js/main.js")
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
+}
+
+function watching() {
+  watch(["app/scss/style.scss"], styles);
+  watch(["app/js/main.js"], script);
+  watch(["app/*.html"]).on("change", browserSync.reload);
+}
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: "app/",
+    },
+  });
+}
+
+exports.script = script;
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.default = parallel(styles, script, browsersync, watching);
